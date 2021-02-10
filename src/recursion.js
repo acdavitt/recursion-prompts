1b9fdc3fc1ed7f4fcbacde3213e0487ac4cfee6e
Amanda Davitt <acdavitt@gmail.com>
2021-02-09 21:05:02 -0600

    Completed recursion problems 1-5

--- src/recursion.js
+++ src/recursion.js
@@ -7,26 +7,88 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
+
 var sum = function(array) {
+  var copy = array.slice()
+  var total = 0;
+  if (copy.length === 0) {
+    return 0;
+  }
+
+  total += copy.shift() + sum(copy);
+  return total;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+ 
+  var total = 0;
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (!Array.isArray(array)) {
+    return array;
+  };
+  var copy = array.slice();
+
+  while (typeof copy[0] === 'number') {
+    total += copy.shift();
+  }
+ 
+  for (var subArr of copy) {
+    total += arraySum(subArr);
+  }
+  
+  return total;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  n = Math.abs(n);
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  }
+  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  
+  var isNegative = false;
+  
+  if (n < 0) {
+    n = Math.abs(n);
+    isNegative = true;
+  }
+ 
+  if (n === 0) {
+    return 0;
+  }
+
+  if (isNegative) {
+    return -((n - 1) + sumBelow(n - 1));
+  }
+  
+  return (n - 1) + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
